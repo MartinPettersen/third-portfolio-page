a698e1e0ef19d9f21343ea99f5c9f8a72f6af768
MartinPettersen <searchingtheskyfor@gmail.com>
2022-12-02 21:51:58 +0100

    test 4

--- utils/fetchAbout.ts
+++ utils/fetchAbout.ts
@@ -1,8 +1,11 @@
 import { AboutType } from "../typedefinition";
 
 export const fetchAbout = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getAbout`);
+    const res = await fetch(`https://s8bsauug.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22project%22%5D`);
 
+
+
+    
     const data = await res.json();
     const about: AboutType[] = data.about;
     
