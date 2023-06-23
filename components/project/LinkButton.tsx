7cad128410c232f6fc305995eec66d14e721f6ed
MartinPettersen <searchingtheskyfor@gmail.com>
2023-06-23 12:47:10 +0200

    add open new tab on project link

--- components/project/LinkButton.tsx
+++ components/project/LinkButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 const LinkButton = ({ children, link }: Props) => {
   return (
     <div className="bg-[#2d5b91] hover:bg-orange-400 w-[96px] h-[32px] flex items-center justify-center rounded-md border-[#102136] border-2">
-      <Link className="text-white  " href={link}>
+      <Link className="text-white  " href={link} target="_blank">
         {children}
       </Link>
     </div>
